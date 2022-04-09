b0903120edad9d95429c9ba33e0fe5b0b222297f
Bokus85 <92443083+Bokus85@users.noreply.github.com>
2022-04-09 13:26:39 +0300

    Add files via upload

---
+++ Ping Pong.py
@@ -0,0 +1,26 @@
+#Создай собственный Ping Pong!
+
+from pygame import *
+
+
+class GameSprite(sprite.Sprite):
+    def __init__(se4lf, player_image, player_x, player_y,size_x, size_y,player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        tanki.blit(self.image, (self.rect.x, self.rect.y))
+        
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
